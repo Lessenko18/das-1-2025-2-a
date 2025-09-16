296e7d079ff73162f62cc1082ae796e98ed7947c
Lessenko18 <127155473+Lessenko18@users.noreply.github.com>
2025-09-16 23:03:41 +0000

    Subscriber

--- designpatterns/src/main/java/br/univille/singleton/Singleton.java
+++ designpatterns/src/main/java/br/univille/singleton/Singleton.java
@@ -1,17 +1,30 @@
 package br.univille.singleton;
 
 public class Singleton {
-    // varivel global
+    // atributo estático cria um endereço único, como uma variável global. ele é caregado antes de tudo
     private static Singleton instance;
 
+    private String segredo;
+
+    public String getSegredo() {
+        return segredo;
+    }
+
+    public void setSegredo(String segredo) {
+        this.segredo = segredo;
+    }
+
     private Singleton() {
-        // construtor privado
+        System.out.println("Construtor Singleton");
     }
 
     public static Singleton getInstance() {
+        System.out.println("getInstance");
         if (instance == null) {
+            System.out.println("new Singleton");
             instance = new Singleton();
         }
+
         return instance;
     }
 }
