9f8731c28c28646f2a76ca5f5cb35ccd365e9e25
Lessenko18 <127155473+Lessenko18@users.noreply.github.com>
2025-09-30 23:03:24 +0000

    Consumer e Producer

--- eventos/src/main/java/br/univille/Main.java
+++ eventos/src/main/java/br/univille/Main.java
@@ -1,7 +1,13 @@
 package br.univille;
 
+import com.azure.core.amqp.AmqpTransportType;
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusMessage;
+import com.azure.messaging.servicebus.ServiceBusSenderClient;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Word");
+
     }
 }
